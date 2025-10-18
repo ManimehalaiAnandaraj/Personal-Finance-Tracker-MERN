31bd5371c9c8a34914fad5d2b001ca8cd10d18d9
manimehalaianandaraj <manimehalaiannakodi@gmail.com>
2025-10-18 12:59:01 +0530

    Update: fix delete issue, add DashboardPage and charts

--- src/services/api.js
+++ src/services/api.js
@@ -2,12 +2,9 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  headers: { "Content-Type": "application/json" },
 });
 
-// Attach JWT automatically if exists
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
